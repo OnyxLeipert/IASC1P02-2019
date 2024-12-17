4b9837821cea9b36fe70d0f77ec4564a776156fb
OnyxLeipert <gf24ft@brocku.ca>
2024-12-16 19:47:27 -0500

    Lab9

---
+++ M9.js
@@ -0,0 +1,20 @@
+document.getElementById('one').innerHTML=Date(Mon Oct 17 2011 16:46:18 GMT-0400 (Eastern Daylight Time));
+var todayDate;
+todayDate = new Date(Monsay Dec 16 2024 7:36:06 GMT-0400 (Eastern Daylight Time));
+var "month" = todayDate.getMonth();
+var "day" =todayDate.getDay();
+document.write( "day" + "/" + "month" + "/" + 2024);
+var myAlert = alert("Boo");
+console.log(myAlert);
+
+document.write("Enter life story");
+<form name="form">
+//input here
+</form>
+document.write("Enter Name");
+<form name="form2">
+//name here
+</form>
+<button type="button">Yes!</button>
+<button type="button">No!</button>
+<button type="submit">Submit!</button>
