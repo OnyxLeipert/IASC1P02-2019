bf2830619e1260c78f59493f46ec09cf48827f1c
OnyxLeipert <gf24ft@brocku.ca>
2024-12-17 15:35:04 -0500

    Final assignment

---
+++ finallink2.js
@@ -0,0 +1,18 @@
+
+function ChangeText() { document.getElementById("one").style.fontSize = "120px";
+document.getElementById("one").style.color = "blue";
+document.getElementById("two").style.color = "green";
+document.getElementById("three").style.color = "orange";
+document.getElementById("three").innerHTML = "PARAGRAPH THREE";
+document.getElementById("two").position = "left"
+}
+function Addition() { document.getElementById("Numbertime").innerHTML = "4";
+}
+function Subtraction() { document.getElementById("Numbertime").innerHTML = "-30";
+}
+function Division() { document.getElementById("Numbertime").innerHTML = "3";
+}
+function Multiplication() { document.getElementById("Numbertime").innerHTML = "8";
+}
+function BestNumber() { document.getElementById("Numbertime").innerHTML = "9";
+}
